391f322f5da40acc5499a715e8c5960dd4eead5d
nepalunisha <146646751+nepalunisha@users.noreply.github.com>
2025-11-30 13:15:53 +0545

    StaffDashboard.tsx

--- src/pages/StaffDashboard.tsx
+++ src/pages/StaffDashboard.tsx
@@ -85,7 +85,7 @@ const StaffDashboard = () => {
       </div>
 
       {/* Patient Selection */}
-      <div className="w-70 min-h-md mt-6 flex flex-col items-center gap-2">
+      <div className="w-full min-h-md mt-6 flex flex-col items-center gap-2">
         {!selectedPatient ? (
           <div className="relative w-full">
             <Input
@@ -109,7 +109,7 @@ const StaffDashboard = () => {
             )}
           </div>
         ) : (
-          <div className="w-full p-3 border border-[#1ebac1] rounded-lg shadow-md bg-white space-y-1">
+          <div className="w-full p-3 border border-[#1ebac1] rounded-xl shadow-md bg-white space-y-1">
             <p className="text-sm text-[#1ebac1] text-center"><strong>ID:</strong> {selectedPatient.id}</p>
             <p className="text-sm text-[#1ebac1] text-center"><strong>Name:</strong> {selectedPatient.name}</p>
             <Button
@@ -123,13 +123,16 @@ const StaffDashboard = () => {
         )}
 
          {/* Billing Dates */}
-        <div className="w-full max-w-md mt-6 flex justify-center gap-6">
+        <div className="w-full max-w-md  gap-2">
+
+
           {/* Start Date */}
-          <div className="flex flex-col items-center">
-            <p className="text-xs text-[#1ebac1] mb-1 font-semibold text-center"> Billing Start Date (BS)</p>
-            <div className="relative w-32 overflow-visible">
+          <div className="flex flex-col mt-4">
+            <p className="text-sm text-[#1ebac1] mb-1 font-semibold "> Billing Start Date (BS)</p>
+            <div className="relative w-full overflow-visible">
+              
               <NepaliDatePicker
-                inputClassName="w-full h-10 text-sm border border-[#1ebac1] rounded-md shadow px-2 text-left bg-white cursor-pointer"
+                inputClassName="w-full h-12 text-sm border border-[#1ebac1] rounded-xl shadow px-2 text-left bg-white cursor-pointer"
                 value={startDate}
                 onChange={(value: string) => setStartDate(value)}
                 options={{ calenderLocale: "ne", valueLocale: "en" }}
@@ -139,14 +142,15 @@ const StaffDashboard = () => {
           </div>
 
           {/* End Date */}
-          <div className="flex flex-col items-center">
-            <p className="text-xs text-[#1ebac1] mb-1 font-semibold text-center">Billing End Date (BS)</p>
-            <div className="relative w-32 overflow-visible">
+          <div className="flex flex-col mt-4">
+            <p className="text-sm text-[#1ebac1] mb-1 font-semibold ">Billing End Date (BS)</p>
+            <div className="relative w-full overflow-novisible">
               <NepaliDatePicker
-                inputClassName="w-full h-10 text-sm border border-[#1ebac1] rounded-md shadow px-2 text-left bg-white cursor-pointer"
+                inputClassName="w-full h-12 text-sm border border-[#1ebac1] rounded-xl shadow px-2 text-left bg-white cursor-pointer"
                 value={endDate}
                 onChange={(value: string) => setEndDate(value)}
                 options={{ calenderLocale: "ne", valueLocale: "en" }}
+                
               />
               <Calendar className="absolute right-2 top-1/2 -translate-y-1/2 h-4 w-4 text-[#1ebac1] pointer-events-none" />
             </div>
@@ -158,7 +162,7 @@ const StaffDashboard = () => {
         {/* Prepare Claim */}
         <div className="w-full max-w-md mt-6">
           <Button
-            className="w-full bg-[#1ebac1] py-3 text-white text-base font-semibold rounded-lg shadow-md hover:scale-105 transition-transform"
+            className="w-full bg-[#1ebac1] py-3 text-white text-base font-semibold rounded-xl shadow-md hover:[#1ebac1] hover:scale-105  transition-transform"
             onClick={proceedToPrepareClaim}
           >
             Prepare Claim
